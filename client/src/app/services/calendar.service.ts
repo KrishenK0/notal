eb0d365cd234796c0862f3e5d4401c69ffcbd18c
krishenk0 <fox2581kriskenk@gmail.com>
2023-12-19 14:29:57 +0100

    switch to angular 17

--- client/src/app/services/calendar.service.ts
+++ client/src/app/services/calendar.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs';
 import { map } from 'rxjs/internal/operators/map';
@@ -11,6 +11,18 @@ export class CalendarService {
   constructor(private http: HttpClient) { }
 
   week(login : {username: string, password: string}) {
-    return this.http.post("http://localhost:8080/week", JSON.stringify(login)).subscribe(res => console.log);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/x-www-form-urlencoded'
+      })
+    };    
+
+    let formData = new HttpParams({
+      fromObject: login
+    });
+
+
+    return this.http.post("http://localhost:8080/week", formData, httpOptions).subscribe(res => console.log);
   }
 }
