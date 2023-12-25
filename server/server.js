39d5c3e9ebe6abcb6c2c2340589c577e672daa42
KrishenK <fox2581kriskenk@gmail.com>
2023-12-25 23:39:00 +0100

    fix minor

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const express = require("express")
 const app = express()
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const serverless = require('serverless-http');
 const { Worker, isMainThread, parentPort } = require('worker_threads');
 var winston = require('winston'),
     expressWinston = require('express-winston');
